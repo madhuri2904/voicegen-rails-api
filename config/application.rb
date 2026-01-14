32b49625b52e886ba0ded522a3538270915f97ee
madhuri2904 <banoriyamadhuri@gmail.com>
2026-01-14 13:58:06 +0000

    fix: disable rails host authorization globally for railway

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module VoicegenRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts.clear
   end
 end
